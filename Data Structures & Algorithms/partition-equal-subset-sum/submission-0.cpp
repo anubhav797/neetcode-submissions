8f0c0c599cd349ae3e6f9704605d7b1ac1045aa7
anubhav797 <anubhavmittal727@gmail.com>
2026-07-31 11:05:53 +0530

    Add: partition-equal-subset-sum - submission-0

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+
+private:
+    bool f(int ind , int target , vector<int> &nums , vector<vector<int>> &dp){
+        if(target == 0){
+            return true;
+        }
+
+        if(ind == 0){
+            return (nums[0] == target);
+        }
+
+        if(dp[ind][target] != -1){
+            return dp[ind][target];
+        }
+
+        bool nonTake = f(ind - 1 , target , nums , dp);
+
+        bool take = false;
+        if(target >= nums[ind]){
+            take = f(ind - 1 , target - nums[ind] , nums , dp);
+        }
+
+        return dp[ind][target] = take | nonTake;
+    }    
+
+public:
+    bool canPartition(vector<int>& nums) {
+
+        int n = nums.size();
+        int sum = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            sum += nums[i];
+        }
+
+        if(sum % 2 == 1) return false;
+
+        int target = sum / 2;
+
+        vector<vector<int>> dp(n , vector<int>(target + 1 , -1));
+
+        return f(n-1 , target , nums , dp);
+    }
+};
