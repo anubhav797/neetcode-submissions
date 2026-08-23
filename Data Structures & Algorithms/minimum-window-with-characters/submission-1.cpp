ecae63f053a73856e8374b77327dbb892e4c5c8e
anubhav797 <anubhavmittal727@gmail.com>
2026-08-23 18:25:54 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+
+    bool contains(vector<int>& maps, vector<int>& mapT) {
+        for(int i = 0; i < 256; i++) {
+            if(mapT[i] > maps[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    string minWindow(string s, string t) {
+
+        vector<int> mapS(256, 0);
+        vector<int> mapT(256, 0);
+
+        int left = 0;
+        int right = 0;
+
+        int minLength = INT_MAX;
+        int startIndex = 0;
+
+        // Store character occurrences of t
+        for(int i = 0; i < t.size(); i++) {
+            mapT[t[i]]++;
+        }
+
+        // Work on string s
+        while(right < s.size()) {
+
+            // Add right character to the window
+            mapS[s[right]]++;
+
+            // Shrink and calulating the window if its valid
+            while(contains(mapS, mapT)) {
+
+                if(right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    startIndex = left;
+                }
+
+                // Remove left character and shrinking the window
+                mapS[s[left]]--;
+                left++;
+            }
+
+            right++;
+        }
+
+        if(minLength == INT_MAX) {
+            return "";
+        }
+
+        return s.substr(startIndex, minLength);
+    }
+};
