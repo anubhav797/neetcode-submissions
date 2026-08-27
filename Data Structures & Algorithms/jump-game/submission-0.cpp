0b04df5b2291136e9d4932efda3a5744aa300a9a
anubhav797 <anubhavmittal727@gmail.com>
2026-08-27 17:23:29 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int n = nums.size();
+        int maxIndex = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(i > maxIndex){
+                return false;
+            }
+            else{
+                maxIndex = max(maxIndex , i + nums[i]);
+            }
+        }
+
+        return true;
+        
+    }
+};
